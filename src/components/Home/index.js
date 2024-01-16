f42fb97613b583dcab66e03811d00806496db6e8
AndrewJones98 <andrewbjones9@gmail.com>
2024-01-16 12:45:24 +1100

    initial commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,21 @@
+import './index.scss';
+
+
+const Home = () => {
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    Andrew Basil-Jones<br/><br/> Software Engineer
+                </h1>
+                <h2>
+                    Passionate About Building
+                </h2>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
