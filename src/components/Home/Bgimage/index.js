a9a6e9d8556295220babf13a6f5658c9916c0c0a
AndrewJones98 <andrewbjones9@gmail.com>
2024-01-17 18:00:41 +1100

    background image

---
+++ src/components/Home/Bgimage/index.js
@@ -0,0 +1,13 @@
+import './index.scss'
+import Cube from '../../../assets/images/Cube.png'
+
+const Bgimage = () => {
+
+    return (
+        <div className='img-container'>
+            <img className='solid-image' src={Cube} alt='C'/>
+        </div>
+    )
+}
+
+export default Bgimage
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './index.scss';
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters';
+import Bgimage from './Bgimage';
 
 
 const Home = () => {
@@ -36,7 +37,7 @@ const Home = () => {
                     PROJECTS
                 </Link>
             </div>
-
+            <Bgimage/>
         </div>
     )
 }
